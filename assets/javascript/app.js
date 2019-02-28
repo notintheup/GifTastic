53f25d2b1cd1288657dee3113de41964cde03831
notintheup <brianjandron@gmail.com>
2019-02-28 17:20:16 -0500

    gippie done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,7 @@ function renderButtons() {
 function displayGiphy() {
 
   var automobile = $(this).attr("dumb-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + automobile + "&api_key=wMugU26h5tWAOA89ljDxznaOctDgfETt&limit=8";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + automobile + "&api_key=wMugU26h5tWAOA89ljDxznaOctDgfETt&limit=10";
   console.log(queryURL)
 
   // Ajax call to giphy API
@@ -30,27 +30,38 @@ function displayGiphy() {
       // $("#giphy-view").empty();
 
       for (var i = 0; i < response.data.length; i++) {
-        // console.log(response.data[i])
-
-        var autoDiv = $("<p>");
+        console.log(response.data[i])
         var giphyURL = response.data[i].images.fixed_height_small_still.url;
+        var gifDiv = $('<div class=gifs>');
         var newImg = $("<img>");
         newImg.attr("src", giphyURL);
         newImg.attr("data-still", response.data[i].images.fixed_height_small_still.url);
         newImg.attr("data-animate", response.data[i].images.fixed_height_small.url);
+        newImg.attr("rating", response.data[i].rating)
         newImg.attr("data-state", "still");
         newImg.addClass("image");
-        autoDiv.append(newImg);
-        $("#giphy-view").append(newImg);
-      }
-      // var autoDiv = $("<divCar>");
-      // var ratings = $("<p>").text("ratings: " + results[i].rating);
-      // autoDiv.append("#giphy-view");
+        gifDiv.append(newImg);
+        var ratings = $("<p>").text("rating: " + response.data[i].rating);
+        gifDiv.append(ratings);
+        $("#giphy-view").append(gifDiv);
 
+      }
     })
 }
 // Click handlers start now
 $(document).on("click", ".auto-btn", displayGiphy);
+
+// Handler of animation
+$(document).on('click', 'img', function(){
+	var state = $(this).attr('data-state');
+		if ( state == 'still'){
+                $(this).attr('src', $(this).data('animate'));
+                $(this).attr('data-state', 'animate');
+            }else{
+                $(this).attr('src', $(this).data('still'));
+                $(this).attr('data-state', 'still');
+            };
+});
 
 //Click event to add a button based on input value
 $("#add-giphy").on("click", function (event) {
