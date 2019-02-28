40a0bce212d2e656d31a1229bdff9a8a95a4b165
notintheup <brianjandron@gmail.com>
2019-02-28 07:22:13 -0500

    giphies

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,17 +7,19 @@ function renderButtons() {
   for (var i = 0; i < topics.length; i++) {
     var a = $("<button>");
     a.addClass("auto-btn");
-    a.attr("data-name", topics[i]);
+    a.attr("dumb-name", topics[i]);
     a.text(topics[i]);
     $(".button-view").append(a);
+    $("#lookup-value").empty();
   }
 }
 
 //Function to display giphers
 function displayGiphy() {
 
-  var automobile = $(topics).attr("#data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + automobile + "&api_key=wMugU26h5tWAOA89ljDxznaOctDgfETt&limit=3";
+  var automobile = $(this).attr("dumb-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + automobile + "&api_key=wMugU26h5tWAOA89ljDxznaOctDgfETt&limit=8";
+  console.log(queryURL)
 
   // Ajax call to giphy API
   $.ajax({
@@ -25,35 +27,30 @@ function displayGiphy() {
       method: "GET"
     })
     .done(function (response) {
+      // $("#giphy-view").empty();
 
-      // $(".giphy-view").empty();
-      // var results = response.data[i];
-      // console.log(response.data);
-      for (var i = 0; i < response.length; i++) {
-        console.log(i);
-        // var autoDiv = $("<div>");
-        //   var rating = response.rating;
-        //   var propOne = $("<p>").text("Rating: " + rating);
-        //   autoDiv.append(propOne);
-        //   console.log(propOne)
-        //   var giphyImg = $("<img>");
-        //   giphyImg.attr("src", results[i].images.fixed_height_small_still.url);
-        //   giphyImg.attr("data-still", results[i].images.fixed_height_small_still.url);
-        //   giphyImg.attr("data-animate", results[i].images.fixed_height_small_still.url);
-        //   giphyImg.attr("data-state", "still");
-        //   giphyImg.addClass("image");
-        //   autoDiv.append(giphyImg);
+      for (var i = 0; i < response.data.length; i++) {
+        // console.log(response.data[i])
 
+        var autoDiv = $("<p>");
+        var giphyURL = response.data[i].images.fixed_height_small_still.url;
+        var newImg = $("<img>");
+        newImg.attr("src", giphyURL);
+        newImg.attr("data-still", response.data[i].images.fixed_height_small_still.url);
+        newImg.attr("data-animate", response.data[i].images.fixed_height_small.url);
+        newImg.attr("data-state", "still");
+        newImg.addClass("image");
+        autoDiv.append(newImg);
+        $("#giphy-view").append(newImg);
       }
+      // var autoDiv = $("<divCar>");
+      // var ratings = $("<p>").text("ratings: " + results[i].rating);
+      // autoDiv.append("#giphy-view");
 
     })
 }
 // Click handlers start now
-$(document).on("click", ".auto-btn", function (event) {
-  event.preventDefault();
-  // alert("HELLO");
-  displayGiphy();
-})
+$(document).on("click", ".auto-btn", displayGiphy);
 
 //Click event to add a button based on input value
 $("#add-giphy").on("click", function (event) {
