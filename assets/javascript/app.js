c7eefd7e3bbd0a69571a8c8b27fc56df2373755e
notintheup <brianjandron@gmail.com>
2019-03-01 09:14:40 -0500

    pretty close

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,7 +19,7 @@ function displayGiphy() {
 
   var automobile = $(this).attr("dumb-name");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + automobile + "&api_key=wMugU26h5tWAOA89ljDxznaOctDgfETt&limit=10";
-  console.log(queryURL)
+  // console.log(queryURL)
 
   // Ajax call to giphy API
   $.ajax({
@@ -32,17 +32,21 @@ function displayGiphy() {
       for (var i = 0; i < response.data.length; i++) {
         console.log(response.data[i])
         var giphyURL = response.data[i].images.fixed_height_small_still.url;
-        var gifDiv = $('<div class=gifs>');
+        var table = $("<table>");
+        var gifDiv = $("<td class= row>");
         var newImg = $("<img>");
         newImg.attr("src", giphyURL);
         newImg.attr("data-still", response.data[i].images.fixed_height_small_still.url);
         newImg.attr("data-animate", response.data[i].images.fixed_height_small.url);
         newImg.attr("rating", response.data[i].rating)
+        newImg.attr("title", response.data[i].title)
         newImg.attr("data-state", "still");
         newImg.addClass("image");
+        table.append(gifDiv);
         gifDiv.append(newImg);
-        var ratings = $("<p>").text("rating: " + response.data[i].rating);
-        gifDiv.append(ratings);
+        var ratings = $("<p>").text("Rating: " + response.data[i].rating);
+        var title = $("<p>").text("Title: " + response.data[i].title);
+        gifDiv.append(title, ratings);
         $("#giphy-view").append(gifDiv);
 
       }
@@ -52,15 +56,15 @@ function displayGiphy() {
 $(document).on("click", ".auto-btn", displayGiphy);
 
 // Handler of animation
-$(document).on('click', 'img', function(){
-	var state = $(this).attr('data-state');
-		if ( state == 'still'){
-                $(this).attr('src', $(this).data('animate'));
-                $(this).attr('data-state', 'animate');
-            }else{
-                $(this).attr('src', $(this).data('still'));
-                $(this).attr('data-state', 'still');
-            };
+$(document).on('click', 'img', function () {
+  var state = $(this).attr('data-state');
+  if (state == 'still') {
+    $(this).attr('src', $(this).data('animate'));
+    $(this).attr('data-state', 'animate');
+  } else {
+    $(this).attr('src', $(this).data('still'));
+    $(this).attr('data-state', 'still');
+  };
 });
 
 //Click event to add a button based on input value
